b93542c1eae4e78bcf63465beaa3398969e3b369
esindger <esindger@gmail.com>
2018-10-21 09:15:24 +0800

    Airgram v0.1.4

--- src/PouchDBStore.ts
+++ src/PouchDBStore.ts
@@ -6,19 +6,24 @@ const db: PouchDB.Database = createCollection('airgram')
 
 @injectable()
 export default class PouchDBStore<DocT> implements ag.Store<DocT> {
-  public create (id: string, doc: DocT): Promise<DocT> {
-    return db.upsert(id, () => doc).then(() => doc)
+  public async delete (id: string): Promise<void> {
+    try {
+      await db.remove(id)
+    } catch (e) {
+      throw e
+    }
   }
 
-  public async get (key: string): Promise<DocT | null> {
+  public async get (key: string, field?: string): Promise<any> {
     try {
-      return await db.get<DocT>(key)
+      const value = await db.get<DocT>(key)
+      return field ? value[field] : value
     } catch (e) {
       return null
     }
   }
 
-  public async update (id: string, doc: Partial<DocT>): Promise<Partial<DocT>> {
+  public async set (id: string, doc: Partial<DocT>): Promise<Partial<DocT>> {
     let nextDoc
     return db.upsert(id, (currentDoc: DocT) => {
       nextDoc = Object.assign({}, currentDoc, doc)
